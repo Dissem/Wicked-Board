ac02d30511e849814a818e1edf04001d0fdea699
pfafs1 <simonnathanael.pfaeffli@students.bfh.ch>
2014-12-02 17:37:09 +0100

    added service for group, user and label

---
+++ service/src/main/java/ch/bfh/wickedcoders/wickedboard/service/impl/DefaultUserService.java
@@ -0,0 +1,9 @@
+package ch.bfh.wickedcoders.wickedboard.service.impl;
+
+import ch.bfh.wickedcoders.wickedboard.service.UserService;
+
+/**
+ * Created by Simu on 02.12.2014.
+ */
+public class DefaultUserService implements UserService {
+}
